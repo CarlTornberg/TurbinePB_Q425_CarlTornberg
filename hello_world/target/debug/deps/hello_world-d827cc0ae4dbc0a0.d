1b2416db17306195485ac7d5ebb1b36beb593c39
Carl Tornberg <carltornberg@hotmail.com>
2025-10-21 12:45:32 +0200

    Created hello world project

---
+++ hello_world/target/debug/deps/hello_world-d827cc0ae4dbc0a0.d
@@ -0,0 +1,5 @@
+/home/bob/Coding/rust/turbine/hello_world/target/debug/deps/hello_world-d827cc0ae4dbc0a0.d: src/main.rs
+
+/home/bob/Coding/rust/turbine/hello_world/target/debug/deps/hello_world-d827cc0ae4dbc0a0: src/main.rs
+
+src/main.rs:
